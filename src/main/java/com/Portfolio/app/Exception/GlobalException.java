fa69cf0fa8420171e47320616af5e01617368db9
Ajay <gangwar030@gmail.com>
2025-09-24 00:25:05 +0530

    GlobalExceptionHandler

---
+++ src/main/java/com/Portfolio/app/Exception/GlobalException.java
@@ -0,0 +1,83 @@
+package com.Portfolio.app.Exception;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.Portfolio.app.Exception.CustomException.PortfolioIdNotFoundException;
+import com.Portfolio.app.Exception.CustomException.UserIdNotFoundException;
+
+@RestControllerAdvice
+public class GlobalException {
+    
+    @ExceptionHandler(UserIdNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> UserNotFoundException(UserIdNotFoundException ux){
+        Map<String, Object> error = new HashMap<>();
+        error.put("Error", "We can not Find the User Id In the Database!");
+        error.put("Message", ux.getMessage());
+        error.put("Status", HttpStatus.NOT_FOUND.value());
+        // error.put("TimeStamp", LocalDateTime.now());
+        error.put("TimeStamp", OffsetDateTime.now());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PortfolioIdNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> PortfolioIdNotFoundException(PortfolioIdNotFoundException px){
+        Map<String, Object> err = new HashMap<>();
+        err.put("Message", "ID NOT FOUND");
+        err.put("Error", "Sorry ! We could not found the Portfolio id ");
+        err.put("Status", HttpStatus.NOT_FOUND.value());
+        // err.put("TimeStamp", LocalDateTime.now());
+        err.put("TimeStamp", OffsetDateTime.now());
+        return new ResponseEntity<>(err, HttpStatus.NOT_FOUND);
+    }
+
+
+    // Handle Validation Exception...
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> ValidationException(MethodArgumentNotValidException ve){
+        Map<String, Object> err = new HashMap<>();
+        err.put("Message", "VALIDATION FAILED");
+        err.put("Error", "Please Enter Valid Data");
+        err.put("Status", HttpStatus.UNAUTHORIZED.value());
+        // err.put("TimeStamp", LocalDateTime.now());
+        err.put("TimeStamp", OffsetDateTime.now());
+        
+        return new ResponseEntity<>(err, HttpStatus.UNAUTHORIZED);
+
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, Object>> ViolationException(DataIntegrityViolationException deve){
+        Map<String, Object> er = new HashMap<>();
+
+        er.put("Message", "Please Check Post Method, Might you are Trying to set null value");
+        er.put("Error", "Data Integrity Voilation Exception Occured !");
+        er.put("Status", HttpStatus.CONFLICT.value());
+        er.put("TimeStamp", LocalDateTime.now());
+
+        return new ResponseEntity<>(er, HttpStatus.CONFLICT);
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> EvergreenExceptionHandler(Exception ex){
+        Map<String, Object> Allerr = new HashMap<>();
+
+        Allerr.put("Message", "Service Down, Please Try Again!");
+        Allerr.put("Error", "INTERNAL SERVER ERROR");
+        Allerr.put("Status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        Allerr.put("TimeStamp", LocalDateTime.now());
+
+        return new ResponseEntity<>(Allerr, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
