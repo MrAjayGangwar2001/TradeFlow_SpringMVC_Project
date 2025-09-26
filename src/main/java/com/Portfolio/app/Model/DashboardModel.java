da35f91eb1eeda707f0631831117c38eefeb2f4f
Ajay <gangwar030@gmail.com>
2025-09-27 01:33:43 +0530

    DashboardModel

---
+++ src/main/java/com/Portfolio/app/Model/DashboardModel.java
@@ -0,0 +1,62 @@
+package com.Portfolio.app.Model;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DashboardModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String assetname;
+
+    // private Double quantity;
+
+    private Double price;
+    
+    @Column(nullable = false, updatable = false)
+    // private LocalDateTime createdAt;
+    private OffsetDateTime createdAt;
+    
+    @Column(nullable = false, updatable = false)
+    // private LocalDateTime updatedAt;
+    private OffsetDateTime updatedAt;
+
+    //Auto Set
+    @PrePersist
+    protected void onCreate(){
+        // this.createdAt = LocalDateTime.now();
+        this.createdAt = OffsetDateTime.now();
+        this.updatedAt = OffsetDateTime.now();
+    }
+
+    // Auto update
+    @PreUpdate
+    protected void onUpdate(){
+        // this.updatedAt = LocalDateTime.now();
+        this.updatedAt = OffsetDateTime.now();
+    }
+
+
+
+    @ManyToMany(mappedBy = "dashboard")
+    private List<UserModel> users;
+}
