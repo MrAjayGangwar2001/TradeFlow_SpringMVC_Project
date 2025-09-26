dc6eb7aa674f991e02a5b0935f05ae77ebf55364
Ajay <gangwar030@gmail.com>
2025-09-27 01:31:03 +0530

    DashboardIdNotFoundEx.

---
+++ src/main/java/com/Portfolio/app/Exception/CustomException/DashboardIdNotFoundException.java
@@ -0,0 +1,8 @@
+package com.Portfolio.app.Exception.CustomException;
+
+public class DashboardIdNotFoundException extends RuntimeException{
+
+        public DashboardIdNotFoundException() {
+                super();
+        }
+}
