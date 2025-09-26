6ccd63d1b3040a0497c0f65507e0c8b5f691e554
Ajay <gangwar030@gmail.com>
2025-09-27 01:33:04 +0530

    UserModel

--- src/main/java/com/Portfolio/app/Model/UserModel.java
+++ src/main/java/com/Portfolio/app/Model/UserModel.java
@@ -1,9 +1,15 @@
 package com.Portfolio.app.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,4 +26,15 @@ public class UserModel {
     private Long id;
     private String name;
     private String email;
+
+
+
+
+    @ManyToMany
+    @JoinTable(
+    name = "UserDashboard",
+    joinColumns = @JoinColumn(name = "Userid"),
+    inverseJoinColumns = @JoinColumn(name = "DashboardId"))
+    private List<DashboardModel> dashboard = new ArrayList<>();
+    
 }
