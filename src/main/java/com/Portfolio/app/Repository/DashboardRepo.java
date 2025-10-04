2cc2669c85b5086ebc48aa9f0c4b39b0076a2017
Ajay <gangwar030@gmail.com>
2025-10-04 23:20:15 +0530

    DashboardRepo

--- src/main/java/com/Portfolio/app/Repository/DashboardRepo.java
+++ src/main/java/com/Portfolio/app/Repository/DashboardRepo.java
@@ -1,5 +1,7 @@
 package com.Portfolio.app.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.Portfolio.app.Model.DashboardModel;
 @Repository
 public interface DashboardRepo extends JpaRepository<DashboardModel, Long> {
 
+    Optional<DashboardModel> findByAssetName(String assetName);
 }
