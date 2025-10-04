0f9ea839c99ec78b3af7130e06b440d9217108ec
Ajay <gangwar030@gmail.com>
2025-10-05 02:13:36 +0530

    WalletRepo

---
+++ src/main/java/com/Portfolio/app/Wallet/WalletRepository.java
@@ -0,0 +1,9 @@
+package com.Portfolio.app.Wallet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WalletRepository extends JpaRepository<WalletModel, Long> {
+
+}
