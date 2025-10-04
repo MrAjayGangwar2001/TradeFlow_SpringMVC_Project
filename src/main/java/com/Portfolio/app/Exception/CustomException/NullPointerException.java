f06fdddb25f31ccacea00043691a436dd97986bd
Ajay <gangwar030@gmail.com>
2025-10-04 23:17:11 +0530

    NullPointerEx

---
+++ src/main/java/com/Portfolio/app/Exception/CustomException/NullPointerException.java
@@ -0,0 +1,8 @@
+package com.Portfolio.app.Exception.CustomException;
+
+public class NullPointerException extends RuntimeException {
+
+    public NullPointerException(String msg){
+        super(msg);
+    }
+}
