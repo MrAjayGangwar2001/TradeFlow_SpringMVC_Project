edf6e61fa0bf47f73dd9c463d688eb50409f5311
Ajay <gangwar030@gmail.com>
2025-10-07 02:17:26 +0530

    PortfolioControll

--- src/main/java/com/Portfolio/app/Portfolio/PortfolioController.java
+++ src/main/java/com/Portfolio/app/Portfolio/PortfolioController.java
@@ -1,8 +1,29 @@
 package com.Portfolio.app.Portfolio;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Portfolio.app.Order.OrderDto;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/portfolio")
 public class PortfolioController {
 
+    private final PortfolioService portService;
+
+    @PostMapping("/data/{Id}")
+    public PortfolioDto PortfolioUpdate(@RequestBody OrderDto orderDto, @PathVariable Long Id ) throws Exception {
+       
+        
+        return portService.PortfolioUpdate(Id, orderDto);
+    }
+    
 }
