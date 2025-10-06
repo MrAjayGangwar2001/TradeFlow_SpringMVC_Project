567e6774db0d58efb621ad0f3fe23c9359e78cc6
Ajay <gangwar030@gmail.com>
2025-10-07 00:42:13 +0530

    WalletTransactionService

---
+++ src/main/java/com/Portfolio/app/WalletTransaction/TransactionService.java
@@ -0,0 +1,36 @@
+package com.Portfolio.app.WalletTransaction;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.Portfolio.app.Enum.TransactionType;
+import com.Portfolio.app.Wallet.WalletModel;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+
+    private final TransactionRepository transRepo;
+
+    public void RecordTransaction(WalletModel wm, Double amount, TransactionType Deposit, String desc) {
+
+        TransactionModel tm = new TransactionModel();
+        tm.setWallet(wm);
+        tm.setAmount(amount);
+        // tm.setType(TransactionType.Deposit);
+        tm.setType(Deposit);
+        tm.setDescription(desc);
+
+        transRepo.save(tm);
+
+    }
+
+    public List<TransactionDto> GetTransactionByWalletId(Long Id) {
+
+        return transRepo.findByWalletId(Id);
+    }
+
+}
