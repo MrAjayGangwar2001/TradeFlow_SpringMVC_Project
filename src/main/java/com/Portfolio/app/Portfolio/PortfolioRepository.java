345a340f24b68ec3208ff3b88545d07b785e5d37
Ajay <gangwar030@gmail.com>
2025-10-06 22:55:40 +0530

    PortfolioRepository

---
+++ src/main/java/com/Portfolio/app/Portfolio/PortfolioRepository.java
@@ -0,0 +1,11 @@
+package com.Portfolio.app.Portfolio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PortfolioRepository extends JpaRepository<PortfolioModel, Long>{
+
+    PortfolioModel findByUserIdandAssetId(Long id, Long id2);
+
+}
