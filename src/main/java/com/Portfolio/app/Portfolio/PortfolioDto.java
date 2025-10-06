c6eedd53fd56e142fd3cc41d977007bed54dbc19
Ajay <gangwar030@gmail.com>
2025-10-07 01:48:11 +0530

    PortfolioDto

--- src/main/java/com/Portfolio/app/Portfolio/PortfolioDto.java
+++ src/main/java/com/Portfolio/app/Portfolio/PortfolioDto.java
@@ -1,5 +1,15 @@
 package com.Portfolio.app.Portfolio;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class PortfolioDto {
 
+    private Long portfolioId;
+    private Double totalQuantity;
+    private Double avgBuyPrice;
 }
