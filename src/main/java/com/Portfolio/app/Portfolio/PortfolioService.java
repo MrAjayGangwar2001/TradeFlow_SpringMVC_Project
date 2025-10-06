7c807fe12d51512e3dd54f74ce58d9316336ac26
Ajay <gangwar030@gmail.com>
2025-10-07 02:11:07 +0530

    PortfolioService

--- src/main/java/com/Portfolio/app/Portfolio/PortfolioService.java
+++ src/main/java/com/Portfolio/app/Portfolio/PortfolioService.java
@@ -1,13 +1,12 @@
 package com.Portfolio.app.Portfolio;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
 
 import org.springframework.stereotype.Service;
 
 import com.Portfolio.app.Model.DashboardModel;
 import com.Portfolio.app.Model.UserModel;
+import com.Portfolio.app.Order.OrderDto;
 import com.Portfolio.app.Repository.DashboardRepo;
 import com.Portfolio.app.Repository.UserRepo;
 
@@ -22,20 +21,27 @@ public class PortfolioService {
     private final DashboardRepo dashboardRepo;
 
 
-    public PortfolioDto PortfolioUpdate(Long Id, Long Id, Double totalQuantity, Double avgBuyPrice){
+    // public PortfolioDto PortfolioUpdate(Long Id, String assetName, Double totalQuantity, Double avgBuyPrice) throws Exception{
+    // public PortfolioDto PortfolioUpdate(Long Id, String assetName, OrderDto orderDto) throws Exception{
+    public PortfolioDto PortfolioUpdate(Long Id,OrderDto orderDto) throws Exception{
 
         UserModel user = userRepo.findById(Id).orElseThrow(() -> new Exception("User Id Not Found"));
-        DashboardModel dashM = dashboardRepo.findById(Id).orElseThrow(() -> new Exception("Asset Id Not Found"));
+        // DashboardModel dashM = dashboardRepo.findById(Id).orElseThrow(() -> new Exception("Asset Id Not Found"));
+        // DashboardModel dashM = dashboardRepo.findByAssetName(assetName).orElseThrow(() -> new Exception("Asset Not Found"));
+        DashboardModel dashM = dashboardRepo.findByAssetName(orderDto.getAssetName()).orElseThrow(() -> new Exception("Asset Not Found"));
 
-        PortfolioModel userExist = portfolioRepo.findByUserIdandAssetId(Id, Id);
+        PortfolioModel userExist = portfolioRepo.findByUserIdandAssetId(user.getId(), dashM.getId());
+
+        double AssetQuantity = orderDto.getAssetQuantity();
+        double AssetBuyPrice = dashM.getPrice();
 
         if (userExist == null) {
             
             PortfolioModel pm = new PortfolioModel();
             pm.setUser(user);
             pm.setDashboard(dashM);
-            pm.setTotalQuantity(totalQuantity);
-            pm.setAvgBuyPrice(avgBuyPrice);
+            pm.setTotalQuantity(AssetQuantity);
+            pm.setAvgBuyPrice(AssetBuyPrice);
             pm.setLastUpdated(OffsetDateTime.now());
 
             portfolioRepo.save(pm);
@@ -46,8 +52,20 @@ public class PortfolioService {
             // To Update Existing Portfolio
 
             double TotalCost = userExist.getAvgBuyPrice() * userExist.getTotalQuantity();
-            double Cost = avgBuyPrice * totalQuantity;
-            double TotalAssetQuentity = userExist.getTotalQuantity + totalQuantity
+            double Cost = AssetBuyPrice * AssetQuantity;
+            double TotalAssetQuantity = userExist.getTotalQuantity() + AssetQuantity;
+
+            double NewAvgPrice = (TotalCost + Cost) / TotalAssetQuantity;
+
+
+            userExist.setTotalQuantity(TotalAssetQuantity);
+            userExist.setAvgBuyPrice(NewAvgPrice);
+            userExist.setLastUpdated(OffsetDateTime.now());
+
+            portfolioRepo.save(userExist);
+
+            return new PortfolioDto(userExist.getPortfolioId(), userExist.getTotalQuantity(), userExist.getAvgBuyPrice());
+            
         }
     }
 }
