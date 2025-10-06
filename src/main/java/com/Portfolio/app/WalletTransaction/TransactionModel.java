30cd743becccf668ea59ab3798dfb89c3dbc5f3e
Ajay <gangwar030@gmail.com>
2025-10-07 00:40:20 +0530

    WalletTransactionEntity

---
+++ src/main/java/com/Portfolio/app/WalletTransaction/TransactionModel.java
@@ -0,0 +1,59 @@
+package com.Portfolio.app.WalletTransaction;
+
+import java.time.OffsetDateTime;
+
+import com.Portfolio.app.Enum.TransactionType;
+import com.Portfolio.app.Wallet.WalletModel;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "wallet_transaction")
+public class TransactionModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TransactionType type;
+    private String description;
+
+
+     
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = OffsetDateTime.now();
+    }
+
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "wallet_id", nullable = false)
+    private WalletModel wallet;
+
+
+}
