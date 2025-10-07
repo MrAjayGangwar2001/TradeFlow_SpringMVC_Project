f7fa4fab767ba936d6b9d55a1ef212dea063dc2d
Ajay <gangwar030@gmail.com>
2025-10-08 01:50:17 +0530

    PortfolioController

--- src/main/java/com/Portfolio/app/Portfolio/PortfolioController.java
+++ src/main/java/com/Portfolio/app/Portfolio/PortfolioController.java
@@ -4,13 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Portfolio.app.Order.OrderDto;
+import com.Portfolio.app.Response.PortfolioResponse;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -20,10 +24,14 @@ public class PortfolioController {
     private final PortfolioService portService;
 
     @PostMapping("/data/{Id}")
-    public PortfolioDto PortfolioUpdate(@RequestBody OrderDto orderDto, @PathVariable Long Id ) throws Exception {
-       
-        
+    public PortfolioDto PortfolioUpdate(@RequestBody OrderDto orderDto, @PathVariable Long Id) throws Exception {
+
         return portService.PortfolioUpdate(Id, orderDto);
     }
-    
+
+    @GetMapping("/all/{userId}")
+    public List<PortfolioResponse> getPortfolioByUser(@PathVariable Long userId) throws Exception {
+        return portService.getPortfolioByUser(userId);
+    }
+
 }
