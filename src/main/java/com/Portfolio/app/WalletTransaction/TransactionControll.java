88017e1ebe470d022647a32b80d37a1085d07df7
Ajay <gangwar030@gmail.com>
2025-10-08 01:54:46 +0530

    ransactionControll

--- src/main/java/com/Portfolio/app/WalletTransaction/TransactionControll.java
+++ src/main/java/com/Portfolio/app/WalletTransaction/TransactionControll.java
@@ -1,10 +1,23 @@
 package com.Portfolio.app.WalletTransaction;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class TransactionControll {
 
+    private final TransactionService transactionService;
+
+    @GetMapping("/wallet/{walletId}")
+    public List<TransactionDto> findTransactionByWalletId(@PathVariable Long walletId) throws Exception{
+        return transactionService.findTransactionByWalletId(walletId);
+    }
 }
