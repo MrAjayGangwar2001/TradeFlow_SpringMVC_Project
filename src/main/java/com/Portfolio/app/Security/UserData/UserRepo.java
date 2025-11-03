a9bf38667944ed4958e8c660be5379cc2e51f25b
Ajay <gangwar030@gmail.com>
2025-11-04 01:44:36 +0530

    Project Updated

--- src/main/java/com/Portfolio/app/Security/UserData/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.Portfolio.app.Security.UserData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends JpaRepository<User, Long> {
-
-}
