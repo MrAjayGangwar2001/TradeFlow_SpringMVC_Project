cb74ba4ccd4168a9aab6ab9384955f776a6c3949
Ajay <gangwar030@gmail.com>
2025-11-06 23:22:47 +0530

    LoginDto

---
+++ src/main/java/com/Portfolio/app/Security/UserData/LoginDto.java
@@ -0,0 +1,12 @@
+package com.Portfolio.app.Security.UserData;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginDto {
+
+    private String email;
+    private String password;
+}
