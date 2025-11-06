e85426224dc049d5c74100e5285337e50c0be758
Ajay <gangwar030@gmail.com>
2025-11-06 23:24:36 +0530

    UpdatedFile

--- src/main/java/com/Portfolio/app/Security/UserData/UserReposit.java
+++ src/main/java/com/Portfolio/app/Security/UserData/UserReposit.java
@@ -1,8 +1,18 @@
 package com.Portfolio.app.Security.UserData;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserReposit extends JpaRepository<User, Long> {
+
+    @Query("Select x from User x where x.email=:email")
+
+    Optional<User> findByEmail(@Param("email") String email);
+
+    User findByUsername(String userName);
 
 }
